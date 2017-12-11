e020359c59ce28371e46b801dc76ff856f145734
Bert Beckmann <bbeckmann@cardinalpeak.com>
2017-12-10 21:53:45 -0700

    initial commit

---
+++ react-test/src/MyReactComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const MyReactComponent = () => (
+  <RaisedButton label="Default" />
+);
+
+export default MyReactComponent;
+
