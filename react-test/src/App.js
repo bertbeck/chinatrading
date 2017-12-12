5f82e0dcee61037e286767c66a76819f4902c7d5
Bert Beckmann <bbeckmann@cardinalpeak.com>
2017-12-11 17:40:27 -0700

    Master: add youtube component

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -10,6 +10,12 @@ import FlatButton from 'material-ui/FlatButton';
 import Toggle from 'material-ui/Toggle';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import Post from './components/Post'
+import Person from './components/Person'
+
+import posts from './data/posts.json'
+import people from './data/people.json'
+import YouTube from './youtube'
 
 import './App.css';
 
@@ -145,15 +151,155 @@ class App extends Component {
                 </div>
                 <div className="col-md-8">
                   <div className="embed-responsive embed-responsive-16by9">
-                    <iframe title="frame1" className="embedly-embed"
-                            src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2F1BXg4wfB9pA%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1BXg4wfB9pA&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2F1BXg4wfB9pA%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube"
-                            scrolling="no" allowFullScreen="" width="640" height="360" frameBorder="0"></iframe>
+                    <YouTube video="1BXg4wfB9pA" autoplay="0" rel="0" modest="1" width="600"/>
                   </div>
 
                 </div>
               </div>
             </div>
           </div>
+
+          <div className="blog-section padding-style-page">
+            <div className="container">
+              <div className="title-subtitle-section">
+                <h5>The Blog</h5>
+                <h2>Thoughts, musings, and ruminations.</h2>
+              </div>
+
+              <Row>
+                {
+                  posts.map((post, index) => {
+                    return (
+                      <Col md="4" className="mb-5" key={index}>
+                        <Post title={post.title} date={post.date} text={post.title} />
+                      </Col>
+                    )
+                  })
+                }
+              </Row>
+            </div>
+          </div>
+
+          <div className="people-section padding-style-page">
+            <div className="container">
+              <div className="title-subtitle-section">
+                <h3>People</h3>
+                <h2>
+                  Success comes from People and Technology you can rely on <br />
+                  We're based in Boulder, Colorado and can assist locally or remotely <br />
+                  We've got expertise in React Native, Flutter and Backends in C#, Dart and Java <br />
+                  .
+                </h2>
+              </div>
+              {/* <div className=" padding-style-page-video">
+                <div className="row">
+                  <div className="col-md-7">
+                    <div className="embed-responsive embed-responsive-16by9">
+                      <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FabSNo2P9mMM%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DabSNo2P9mMM&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FabSNo2P9mMM%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube" scrolling="no" allowfullscreen="" width="640" height="360" frameborder="0"></iframe>
+                    </div>
+                  </div>
+                  <div className="col-md-5">
+                    <h2>React Native</h2>
+                    <p className="video-text-style">Scaling to 80 Million Users </p>
+                    <p>React Native is making inroads into <br /> mainstream Mobile cross platform <br /> development. </p>
+                  </div>
+                </div>
+              </div>
+              <div className=" padding-style-page-video">
+                <div className="row">
+                  <div className="col-md-5">
+                    <h2>Flutter vs React Native</h2>
+                    <p className="video-text-style">Scaling to 80 Million Users </p>
+                    <p>Mobile developers compare Flutter and React Native </p>
+                  </div>
+                  <div className="col-md-7">
+                    <div className="embed-responsive embed-responsive-16by9">
+                      <iframe  src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FabSNo2P9mMM%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DabSNo2P9mMM&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FabSNo2P9mMM%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube" scrolling="no" allowfullscreen="" width="640" height="360" frameborder="0" className="embedly-embed"></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
+
+              {
+                <Row>
+                  {
+                    people.map((person, index) => {
+                      return (
+                        <Col md="4" className="mb-5" key={index}>
+                          <Person name={person.name} photo={person.photo} role={person.role} description={person.description} />
+                        </Col>
+                      )
+                    })
+                  }
+                </Row>
+              }
+            </div>
+          </div>
+
+          <div className="services-section padding-style-page">
+            <div className="container">
+              <div className="title-subtitle-section">
+                <h3>Services</h3>
+                <h2>Contact us for Mobile and Web Development</h2>
+                <FormInline color="elegant" className="text-center">
+                  <Input label=" Name"  group type="text" />
+                  <Input label="Email"  group type="email" />
+                  <Button color="elegant" >Submit</Button>
+                </FormInline>
+              </div>
+            </div>
+          </div>
+
+          <div className="contact-section padding-style-page">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                  <h5>Got Questions? </h5>
+                  <h2> We've Got Answers.</h2>
+                  <div className=" ">
+                    <Input label="Name"  group type="text" />
+                    <Input label="Email"  group type="email" />
+                    <Input type="textarea" label="Basic textarea"/>
+                    <Button color="elegant" className="btn btn-block send-mesage-button">Submit</Button>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="">
+                    <iframe className="google-maps-style" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d48855.52246526886!2d-105.2136303!3d40.0649237!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2srs!4v1512580175503" allowFullScreen={false}></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="about-us-contact-section padding-style-page">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-2">
+                  <address>
+                    <strong> <h5>About Us</h5></strong>
+                    <h6>Our Mission</h6>
+                    <h6>We're Hiring!</h6>
+                  </address>
+                </div>
+                <div className="col-md-2">
+                  <address>
+                    <strong>  <h5>Information</h5></strong>
+                    <h6>Consultants Available</h6>
+                    <h6>Help & Support</h6>
+                  </address>
+                </div>
+                <div className="col-md-2">
+                  <address>
+                    <strong> <h5>Contact</h5></strong>
+                    <h6>720-354-2372</h6>
+                    <h6>bertbeck@gmail.com</h6>
+                  </address>
+                </div>
+              </div>
+            </div>
+          </div>
+
 
           <MyReactComponent/>
         </div>
